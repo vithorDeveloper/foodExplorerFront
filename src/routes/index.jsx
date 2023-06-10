f3acdc3dffc95b5f408ec91fc63e2f807ddeb6f5
vithorDeveloper <vitorsantosdev663@gmail.com>
2023-06-10 16:40:41 -0300

    navegation

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { AppRoutesClient } from "./app.routes.client";
-// import { AppRoutesAdm } from "./app.routes.adm";
+// import { AppRoutesClient } from "./app.routes.client";
+import { AppRoutesAdm } from "./app.routes.adm";
 // import { AuthRoutes } from "./auth.routes";
 
 export function Routes(){
