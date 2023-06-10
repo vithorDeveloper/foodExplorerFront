417b2ea13f8734cb1b32f7101017a9fabbad816a
vithorDeveloper <vitorsantosdev663@gmail.com>
2023-06-10 16:54:21 -0300

    refactored routes

--- src/pages/pagesAdm/EditDish/index.jsx
+++ src/pages/pagesAdm/EditDish/index.jsx
@@ -6,8 +6,8 @@ import { HeaderMobile } from "../../../components/mobile/headerAdm"
 import { HeaderDesktop} from "../../../components/desktop/headerAdm"
 import { FooterMobile } from "../../../components/mobile/footer";
 import { FooterDesktop } from "../../../components/desktop/footer";
-import { NewIngredient } from "../../../components/responsive/newIngredient";
 import { TextButton } from "../../../components/responsive/textButton";
+import { NewIngredient } from "../../../components/responsive/NewIngredient";
 import { Link } from "react-router-dom"
 
 import { FaChevronLeft, FaDownload, FaAngleDown} from "react-icons/fa"
